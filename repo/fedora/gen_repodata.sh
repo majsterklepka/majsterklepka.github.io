612ae45756b3634c014e233b7f865386c48a61d2
Paweł Sobótka <48721262935pl@gmail.com>
2018-10-30 03:30:48 +0100

    update

--- repo/fedora/gen_repodata.sh
+++ repo/fedora/gen_repodata.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+createrepo --update 28/SRPMS
+createrepo --update 28/updates
+createrepo --update 28/x86_64
+
 ./appstream_fedora-28_86_64.sh
 
 ./appstream_fedora-28_86_64-updates.sh
 
 ./appstream_fedora-28_86_64-sources.sh
 
-./data-fedora-28_x86_64.sh
+./data-fedora-28_x86_64.shu
 
 ./data-fedora-28_x86_64-updates.sh
 
