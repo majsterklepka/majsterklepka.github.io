6b46d99780f017396722a5b782b8ef23867b0cfa
Paweł Sobótka <48721262935pl@gmail.com>
2018-11-01 13:21:56 +0100

    update

---
+++ repo/fedora/appstream_fedora-29_86_64-sources.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+appstream-builder				\
+	--origin=majsterklepka-fedora-sources		\
+	--basename=majsterklepka-fedora-sources-29			\
+	--cache-dir=/tmp/asb-cache		\
+	--enable-hidpi				\
+	--max-threads=1				\
+	--min-icon-size=32			\
+	--output-dir=/tmp/asb-md		\
+	--packages-dir=29/SRPMS/
--- repo/fedora/gen_repodata.sh
+++ repo/fedora/gen_repodata.sh
@@ -4,14 +4,33 @@ createrepo --update 28/SRPMS
 createrepo --update 28/updates
 createrepo --update 28/x86_64
 
+createrepo --update 29/SRPMS
+createrepo --update 29/updates
+createrepo --update 29/x86_64
+
+
+
 ./appstream_fedora-28_86_64.sh
 
 ./appstream_fedora-28_86_64-updates.sh
 
 ./appstream_fedora-28_86_64-sources.sh
 
-./data-fedora-28_x86_64.shu
+./data-fedora-28_x86_64.sh
 
 ./data-fedora-28_x86_64-updates.sh
 
 ./data-fedora-28_x86_64-sources.sh
+
+
+./appstream_fedora-29_86_64.sh
+
+./appstream_fedora-29_86_64-updates.sh
+
+./appstream_fedora-29_86_64-sources.sh
+
+./data-fedora-29_x86_64.sh
+
+./data-fedora-29_x86_64-updates.sh
+
+./data-fedora-29_x86_64-sources.sh
